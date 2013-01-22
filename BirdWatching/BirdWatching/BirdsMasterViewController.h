cc9b091dc098d8eb19b7a20720e3eff9394cfe00
BENJAMIN BUCKMASTER <bpbuckmaster@SH308-17.local>
2013-01-22 12:22:38 -0600

    Tutorial through implementing custom initializer method.

---
+++ BirdWatching/BirdWatching/BirdsMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  BirdsMasterViewController.h
+//  BirdWatching
+//
+//  Created by BENJAMIN BUCKMASTER on 1/22/13.
+//  Copyright (c) 2013 BENJAMIN BUCKMASTER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirdsMasterViewController : UITableViewController
+
+@end
