813475e4ecb688ab3bf869f98c67d2408a0b2d7d
BENJAMIN BUCKMASTER <bpbuckmaster@SH308-17.local>
2013-01-28 16:51:42 -0600

    Now "Laying out the first cell in the add scene's table".

---
+++ BirdWatching/BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by BENJAMIN BUCKMASTER on 1/28/13.
+//  Copyright (c) 2013 BENJAMIN BUCKMASTER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
